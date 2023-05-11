50382b61368413badf942587e16b5076857a4b23
Andres Cayambe <andrescayambe99@hotmail.com>
2023-05-11 21:26:24 +0200

    [add]buttons

--- src/components/partials/SectionAbout.vue
+++ src/components/partials/SectionAbout.vue
@@ -27,19 +27,22 @@ export default {
                 
         </div>
         <div class="col">
-          <h4>12 Year Experience </h4>
+          <h4 class="subtitle">12 Year Experience </h4>
           <h2>We Are The Best Gaming Company </h2>
           <p>Nulla quis lorem ut libero malesuada feugiat. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Cras ultricies ligula sed magna dictum porta.Praesent sapien massa, convallis a pellentesque nec, egestas non nisi praesent sapien massa ligula sed magna dictum porta. </p>
           <p>Nulla quis lorem ut libero malesuada feugiat. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Cras ultricies ligulaa. </p>
           <ul>
-            <li><img src="../../assets/image (10).svg" alt="">Sed porttitor lectus nibh. Proin eget tortor risus. </li>
-            <li><img src="../../assets/image (10).svg" alt="">Sed porttitor lectus nibh. Proin eget tortor risus. </li>
-            <li><img src="../../assets/image (10).svg" alt="">Sed porttitor lectus nibh. Proin eget tortor risus. </li>
-            <li><img src="../../assets/image (10).svg" alt="">Sed porttitor lectus nibh. Proin eget tortor risus. </li>
+            <li><svg class="svg-inline--fa fa-check" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M438.6 105.4C451.1 117.9 451.1 138.1 438.6 150.6L182.6 406.6C170.1 419.1 149.9 419.1 137.4 406.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4C21.87 220.9 42.13 220.9 54.63 233.4L159.1 338.7L393.4 105.4C405.9 92.88 426.1 92.88 438.6 105.4H438.6z"></path></svg>Sed porttitor lectus nibh. Proin eget tortor risus. </li>
+            <li><svg class="svg-inline--fa fa-check" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M438.6 105.4C451.1 117.9 451.1 138.1 438.6 150.6L182.6 406.6C170.1 419.1 149.9 419.1 137.4 406.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4C21.87 220.9 42.13 220.9 54.63 233.4L159.1 338.7L393.4 105.4C405.9 92.88 426.1 92.88 438.6 105.4H438.6z"></path></svg>Sed porttitor lectus nibh. Proin eget tortor risus. </li>
+            <li><svg class="svg-inline--fa fa-check" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M438.6 105.4C451.1 117.9 451.1 138.1 438.6 150.6L182.6 406.6C170.1 419.1 149.9 419.1 137.4 406.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4C21.87 220.9 42.13 220.9 54.63 233.4L159.1 338.7L393.4 105.4C405.9 92.88 426.1 92.88 438.6 105.4H438.6z"></path></svg>Sed porttitor lectus nibh. Proin eget tortor risus. </li>
+            <li><svg class="svg-inline--fa fa-check" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M438.6 105.4C451.1 117.9 451.1 138.1 438.6 150.6L182.6 406.6C170.1 419.1 149.9 419.1 137.4 406.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4C21.87 220.9 42.13 220.9 54.63 233.4L159.1 338.7L393.4 105.4C405.9 92.88 426.1 92.88 438.6 105.4H438.6z"></path></svg>Sed porttitor lectus nibh. Proin eget tortor risus. </li>
+
           </ul>
-          <div>
-            <button>discovery more</button>
-            <button>play</button>
+          <div class="buttons">
+            <button class="big-button br-rad">Read More<svg class="svg-inline--fa fa-arrow-right-long" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-right-long" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M502.6 278.6l-128 128c-12.51 12.51-32.76 12.49-45.25 0c-12.5-12.5-12.5-32.75 0-45.25L402.8 288H32C14.31 288 0 273.7 0 255.1S14.31 224 32 224h370.8l-73.38-73.38c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l128 128C515.1 245.9 515.1 266.1 502.6 278.6z"></path></svg></button>
+               <a class="play" href="#">
+                <svg class="svg-inline--fa fa-play" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="play" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" data-fa-i2svg=""><path fill="currentColor" d="M361 215C375.3 223.8 384 239.3 384 256C384 272.7 375.3 288.2 361 296.1L73.03 472.1C58.21 482 39.66 482.4 24.52 473.9C9.377 465.4 0 449.4 0 432V80C0 62.64 9.377 46.63 24.52 38.13C39.66 29.64 58.21 29.99 73.03 39.04L361 215z"></path></svg>
+               </a> 
           </div>
         
         </div>
@@ -107,7 +110,6 @@ export default {
       width: 50%;
       color: $white;
       h4{
-        color: $green;
         padding-bottom: 10px;
       }
       h2{
@@ -115,6 +117,7 @@ export default {
         font-weight: 700;
         padding-bottom: 10px;
       }
+     
 
       p{
         padding: 5px 0;
@@ -127,7 +130,7 @@ export default {
           padding: 8px 0;
           display: flex;
           align-items: center;
-          img{
+          svg{
             color: $green;
             margin-right: 10px;
             width: 30px;
