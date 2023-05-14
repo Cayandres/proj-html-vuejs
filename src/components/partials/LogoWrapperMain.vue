86e4f794bf49949428023d5d828cf11cf36e4e57
Andres Cayambe <andrescayambe99@hotmail.com>
2023-05-14 23:07:39 +0200

    [fix]img

--- src/components/partials/LogoWrapperMain.vue
+++ src/components/partials/LogoWrapperMain.vue
@@ -7,12 +7,12 @@ export default {
 <template>
   <div class="container">
     <div class="images">
-      <div><img src="../../assets/img/1.png" alt=""></div>
-      <div><img src="../../assets/img/2.png" alt=""></div>
-      <div><img src="../../assets/img/3.png" alt=""></div>
-      <div><img src="../../assets/img/4.png" alt=""></div>
-      <div><img src="../../assets/img/5.png" alt=""></div>
-      <div><img src="../../assets/img/6.png" alt=""></div>
+      <div><img src="/img/1.png" alt=""></div>
+      <div><img src="/img/2.png" alt=""></div>
+      <div><img src="/img/3.png" alt=""></div>
+      <div><img src="/img/4.png" alt=""></div>
+      <div><img src="/img/5.png" alt=""></div>
+      <div><img src="/img/6.png" alt=""></div>
     </div>
   </div>
 </template>
